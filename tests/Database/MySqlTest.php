7bddf5432707bdd256f2e4ba8d165a441b25741e
Henrik Volckmer <henrik.volckmer@gmail.com>
2023-07-30 11:22:57 -0400

    Added really basic test for mysql connection

--- tests/Database/MySqlTest.php
+++ tests/Database/MySqlTest.php
@@ -7,16 +7,33 @@ use PHPUnit\Framework\TestCase;
 
 class MySqlTest extends TestCase
 {
-    public function testConnection() {
-        $connection = new Connection([
+    protected Connection $connection;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->connection = new Connection([
             'type' => 'mysql',
             'host' => 'localhost',
             'schema' => 'information_schema',
             'username' => 'root',
             'password' => 'root'
         ]);
+    }
 
-        $result = $connection->query('SELECT 1 as testColumn')->fetchAll();
-        $this->assertEquals(1, count($result));
+    public function testConnection() {
+        $result = $this->connection->query('SELECT 1 as testColumn')->fetchAll();
+        $this->assertCount(1, $result);
+    }
+
+    public function testSuccessfulTransaction() {
+        $number = 12345;
+
+        $this->connection->begin();
+        $resultA = $this->connection->query('SELECT :number as testColumn', ['number' => $number])->fetchRow();
+        $resultB = $this->connection->query('SELECT :number as testColumn', ['number' => $number])->fetchRow();
+        $this->connection->end();
+
+        $this->assertEquals($number * 2, $resultA['testColumn'] + $resultB['testColumn']);
     }
 }
