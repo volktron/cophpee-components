89eba56ee96596e4321a7d5c7c350989794ad6d7
Henrik Volckmer <henrik.volckmer@gmail.com>
2023-08-13 09:43:16 -0400

    html and javascript output types in Controller class

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -4,6 +4,20 @@ namespace Cophpee\Components\Controller;
 
 class Controller
 {
+    protected function html(string $html, int $response_code = 200): void
+    {
+        header('Content-Type: text/html; charset=utf-8');
+        http_response_code($response_code);
+        echo $html;
+    }
+
+    protected function js(string $js, int $response_code = 200): void
+    {
+        header('Content-Type: text/javascript; charset=utf-8');
+        http_response_code($response_code);
+        echo $js;
+    }
+
     protected function json(array $data, int $response_code = 200): void
     {
         header('Content-Type: application/json; charset=utf-8');
