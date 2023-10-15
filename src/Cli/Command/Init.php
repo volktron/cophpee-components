af90383922e747771d13fb761f485b16d107cac3
Henrik Volckmer <henrik.volckmer@gmail.com>
2023-10-15 15:24:06 -0400

    Init command

--- src/Cli/Command/Init.php
+++ src/Cli/Command/Init.php
@@ -5,6 +5,28 @@ namespace Cophpee\Components\Cli\Command;
 class Init extends AbstractCommand
 {
     public function init(): bool {
+        $cwd = getcwd();
+        $vendorDirectory = getcwd() . DIRECTORY_SEPARATOR . 'vendor';
+        $cophpeeDirectory = $vendorDirectory . DIRECTORY_SEPARATOR . 'volktron' . DIRECTORY_SEPARATOR . 'cophpee';
+
+        echo "Initializing new CoPHPee project\n";
+
+        foreach([ // TODO: do a recursive directory scan instead of using hardcoded array of files
+                    'app' . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . 'IndexController.php',
+                    'app' . DIRECTORY_SEPARATOR . 'routes' . DIRECTORY_SEPARATOR . 'routes.php',
+                    'configs' . DIRECTORY_SEPARATOR . 'examples' . DIRECTORY_SEPARATOR . 'databases.php',
+                    'public' . DIRECTORY_SEPARATOR . '.htaccess',
+                    'public' . DIRECTORY_SEPARATOR . 'index.php',
+                    'tests' . DIRECTORY_SEPARATOR . 'endpoints' . DIRECTORY_SEPARATOR . 'IndexControllerTest.php'
+                ] as $file) {
+            copy(
+                $cophpeeDirectory . DIRECTORY_SEPARATOR . $file,
+                $cwd . DIRECTORY_SEPARATOR . $file
+            );
+        }
+
+        echo "Done\n";
+
         return true;
     }
 }
